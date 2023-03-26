7ad38fb873f6b4b3dcd41f1375244ede5adc8eac
4eRePaLLlKa <109215860+4eRePaLLlKa@users.noreply.github.com>
2023-03-26 19:03:06 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from pygame import *
+
+window = display.set_mode((600,500))
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image, player_x,player_y,size_x,size_y,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x,size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+
+    def reset(self):
+        window.blit(sef.image, (self.rect.x, self.rect.y))
+
+
+
+class Player(GameSprite):
+    def update_up(self):
+        keys = key.get_pressed()
+        if keys == [K_UP] and player_y > 600:
+            Player_y + 5 
+
+    
+    def update_down(self):
