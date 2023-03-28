8465f51d520c31b32c46673340938736603698b2
4eRePaLLlKa <109215860+4eRePaLLlKa@users.noreply.github.com>
2023-03-28 21:05:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,83 @@ class GameSprite(sprite.Sprite):
 
 
     def reset(self):
-        window.blit(sef.image, (self.rect.x, self.rect.y))
-
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 
 class Player(GameSprite):
-    def update_up(self):
-        keys = key.get_pressed()
-        if keys == [K_UP] and player_y > 600:
-            Player_y + 5 
+    def update_r(self):
 
-    
-    def update_down(self):
+        keys = key.get_pressed()
+        if keys[K_UP] == True: 
+            self.rect.y = self.rect.y - self.speed 
+
+        if keys[K_DOWN] == True: 
+            self.rect.y = self.rect.y + self.speed 
+
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] == True: 
+            self.rect.y = self.rect.y - self.speed 
+
+        if keys[K_s] == True: 
+            self.rect.y = self.rect.y + self.speed 
+
+window.fill((200,255,255))
+
+game = True
+finish = False
+
+
+clock = time.Clock()
+FPS = 60
+
+
+player1 = Player('stick.png',5,300,20,350,5)
+player2 = Player('stick.png',575,300,20,350,5)
+
+ball = GameSprite('ball.png',300,250,25,25,7)
+
+speed_x = 2
+speed_y = 2
+
+while game:
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    if finish != True:
+        window.fill((200,255,255))
+
+        player1.update_l()
+        player2.update_r()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y < 10 or ball.rect.y > 490:
+            speed_y = speed_y * -1
+
+        if sprite.collide_rect(player1,ball):
+            speed_x *= -1
+            speed_y *= -1        
+
+        if sprite.collide_rect(player2,ball):
+            speed_x *= -1
+            speed_y *= -1      
+
+
+        ball.reset()
+        player1.reset()
+        player2.reset()
+        
+
+
+
+
+
+    display.update()
+    clock.tick(FPS)
+
